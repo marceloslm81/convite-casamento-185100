2eef15e13840c5e6924f45f3beb8dcf17c431bd3
Marcelo Cavalcanti da Silva <marcelo.slm.mc@gmail.com>
2026-08-13 15:40:42 -0300

    dv

--- src/App.jsx
+++ src/App.jsx
@@ -23,7 +23,7 @@ import Login from "./pages/admin/Login";
 import ErrorBoundary from "@/components/ui/error-boundary";
 import RouterErrorBoundary from "@/components/ui/router-error-boundary";
 import DefaultHome from "./pages/Home";
-import PoweredByBadge from "@/components/PoweredByBadge";
+
 
 const { Pages, Layout, mainPage, Admins, adminMainPage, AdminLayout } = pagesConfig;
 
@@ -141,7 +141,6 @@ function App() {
           <RouterProvider router={router} />
           <Toaster />
           <VisualEditAgent />
-          <PoweredByBadge />
         </QueryClientProvider>
       </AuthProvider>
     </ErrorBoundary>
